a8983d204a508bf149a6523392526eb277f2cbff
Carlos Ziegler <carlos.ziegler@gmail.com>
2023-04-23 22:03:04 +0200

    🚀 test(App.test.tsx): add HashRouter to App test to fix failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 describe("App", () => {
   test("Simple test", async (context) => {
-    render(<App />);
+    render(
+      <HashRouter>
+        <App />
+      </HashRouter>
+    );
     const linkElement = screen.getByText(/Console/i);
     expect(linkElement).toBeDefined();
   });
